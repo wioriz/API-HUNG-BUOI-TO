c698d4f03a07675edb69de4f4bf4a5924d7bccf8
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-03 21:30:13 +0800

    Update gaitiktok.js

--- controllers/gaitiktok.js
+++ controllers/gaitiktok.js
@@ -1,1 +1,7 @@
+const { errorHandler } = require("../utils");
 
+exports.gaitiktok = async (req, res, next) => {
+  var data = require('fs-extra').readFileSync(__dirname + '/../gaitiktok.txt', 'utf-8').split('\n');
+  link = data[Math.floor(Math.random() * data.length)].trim();
+  res.json({"Authors":"Random Video Gái TikTok","url":`${link}`});
+};
