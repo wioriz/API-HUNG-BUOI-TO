6d775a96a17e4c10d53ca83036636a27b5c9d132
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-03 16:06:30 +0800

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 const { errorHandler } = require("../utils");
 
 exports.home = async (req, res, next) => {
-  res.json({"Authors":"có đầu buồi"});
+  res.json({"Authors":"API BY ANH HUNG BUOI TO =))"});
 };
