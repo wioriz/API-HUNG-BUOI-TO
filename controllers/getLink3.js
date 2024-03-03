e9f814a893c800cef2391e8685fa70e91a4925f5
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-03 16:18:48 +0800

    Update getLink3.js

--- controllers/getLink3.js
+++ controllers/getLink3.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.getLink3 = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../moto.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"DungUwU && Lê Anh Trí","url":`${link}`});
+  res.json({"Authors":"Buồi","url":`${link}`});
 };
