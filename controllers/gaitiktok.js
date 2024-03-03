1b4ab6c19683f0a523fd29b81e4c3fb5af5314f0
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-03 22:16:59 +0800

    Delete controllers/gaitiktok.js

--- controllers/gaitiktok.js
+++
@@ -1,7 +0,0 @@
-const { errorHandler } = require("../utils");
-
-exports.gaitiktok = async (req, res, next) => {
-  var data = require('fs-extra').readFileSync(__dirname + '/../gaitiktok.txt', 'utf-8').split('\n');
-  link = data[Math.floor(Math.random() * data.length)].trim();
-  res.json({"Authors":"Random Video Gái TikTok","url":`${link}`});
-};
