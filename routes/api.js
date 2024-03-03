851fd2330a213e8056f57534367a837200d6d49c
HUNGXPRO <108609419+HUNGPROXTRICK@users.noreply.github.com>
2024-03-03 21:28:29 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const { getLink2 } = require("../controllers/getLink2");
 const { getLink3 } = require("../controllers/getLink3");
 const { getlink4 } = require("../controllers/getlink4");
 const { videosex } = require("../controllers/videosex");
+const { gaititok } = require("../controllers/gaitiktok");
 const { home } = require("../controllers/home");
 
 router.get("/getLink1", getLink1);
@@ -11,6 +12,7 @@ router.get("/getLink2", getLink2);
 router.get("/getLink3", getLink3);
 router.get("/getlink4", getlink4);
 router.get("/videosex", videosex);
+router.get("/gaitiktok", gaitiktok);
 router.get("/", home);
 
 module.exports = router; 
