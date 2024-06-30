7df326a8a0f065649c68d071665fee0da325ec24
wioriz <mikobot731@gmail.com>
2024-06-30 23:42:03 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const { taixiu } = require("../controllers/taixiu");
 const { vdgai } = require("../controllers/vdgai");
+const { anhgai } = require("../controllers/anhgai");
 const { home } = require("../controllers/home");
 
 router.get("/vdgai", vdgai);
+router.get("/anhgai", anhgai);
 router.get("/", home);
 
 module.exports = router; 
